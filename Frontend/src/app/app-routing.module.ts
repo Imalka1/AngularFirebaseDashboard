9fd493721ac198997bd58f7d5f3add805138f6b2
Imalka Gunawardana <imalkagunawardana1@gmail.com>
2020-04-27 21:44:48 +0530

    up

--- Frontend/src/app/app-routing.module.ts
+++ Frontend/src/app/app-routing.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {DashboardComponent} from "./dashboard/dashboard.component";
+import {NavbarComponent} from "./navbar/navbar.component";
 
 
 const routes: Routes = [
   {
     path:'dashboard',
-    component:DashboardComponent
+    component:NavbarComponent
   },
   {path: '', pathMatch: "full", redirectTo: '/dashboard'}
 ];
